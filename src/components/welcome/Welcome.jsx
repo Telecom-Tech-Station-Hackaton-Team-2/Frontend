4b5bc7155349095458416726b43cfb5b23fa8286
Maxi Giusto <msgiusto@teco.com.ar>
2022-03-17 20:48:04 -0300

    Formato de Welcome terminado

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,49 +1,62 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import './welcome.css'
+import "./welcome.css";
+import capa from './capacitacion.png'
+import access from './accesos.png'
+import orga from './orga.png'
 
 function Welcome() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="card cardWelcome col-md-5 col-12 m-2">
-      <Link exact="true" to="/capacitaciones">
+    return (
+        <div className="container">
+            <div className="row">
+              <div className="col-md-4 col-12 mt-2">
+                <div className="card cardWelcome ">
+                    <Link exact="true" to="/capacitaciones">
+                        <img src={capa} className="card-img-top" alt="foto capacitacion" />
 
-              <img src="..." className="card-img-top" alt="..."/>
-
-              <div className="card-body">
-                <h5 className="card-title">Capacitaciones</h5>
-                <p className="card-text">Aquí podrás encontrar las capacitaciones disponibles junto a las que ya realizaste</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Capacitaciones</h5>
+                            <p className="card-text">
+                                Aquí podrás encontrar las capacitaciones
+                                disponibles junto a las que ya realizaste
+                            </p>
+                        </div>
+                    </Link>
+                </div>
               </div>
-      </Link>
-        </div>
+              <div className="col-md-4 col-12 mt-2">
+                <div className="card cardWelcome">
+                    <Link exact="true" to="/accesos">
+                        <img src={access} className="card-img-top" alt="foto accesos" />
 
-        <div className="card cardWelcome col-md-5 col-12 m-2">
-      <Link exact="true" to="/accesos">
-
-              <img src="..." className="card-img-top" alt="..."/>
-
-              <div className="card-body">
-                <h5 className="card-title">Accesos</h5>
-                <p className="card-text">Aquí podrás encontrar los accesos disponibles junto a las que ya tenés asignados</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Accesos</h5>
+                            <p className="card-text">
+                                Aquí podrás encontrar los accesos disponibles
+                                junto a las que ya tenés asignados
+                            </p>
+                        </div>
+                    </Link>
+                </div>
               </div>
-      </Link>
-        </div>
+              <div className="col-md-4 col-12 mt-2">
+                <div className="card cardWelcome">
+                    <Link exact="true" to="/organigrama">
+                        <img src={orga} className="card-img-top" alt="foto organigrama" />
 
-        <div className="card cardWelcome col-md-5 col-12 m-2">
-      <Link exact="true" to="/organigrama">
-
-              <img src="..." className="card-img-top" alt="..."/>
-
-              <div className="card-body">
-                <h5 className="card-title">Organigrama</h5>
-                <p className="card-text">Aquí podrás encontrar a tus compañeros, jefes y colaboradores con los que te vas a relacionar</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Organigrama</h5>
+                            <p className="card-text">
+                                Aquí podrás encontrar a tus compañeros, jefes y
+                                colaboradores con los que te vas a relacionar
+                            </p>
+                        </div>
+                    </Link>
+                </div>
               </div>
-      </Link>
+            </div>
         </div>
-        </div>
-    </div>
-  )
+    );
 }
 
-export default Welcome
+export default Welcome;
