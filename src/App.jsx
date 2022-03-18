188d97cbcca11f83533afb03985ce2492796fe45
Jose Camblong <eltrujuy@gmail.com>
2022-03-17 23:51:34 -0300

    Se agregan cambios en lista de capacitaciones y cursos. Se crea ruta profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,24 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/footer/Footer";
 import Welcome from "./components/welcome/Welcome";
 import LogingForm from "./components/loginForm/LoginForm";
-import ItemsListContainer from "./components/productList/ItemsListContainer";
+import ItemsListContainer from "./components/itemsList/ItemsListContainer";
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<LogingForm />} />
-                    <Route
-                        path="/capacitaciones"
-                        element={<ItemsListContainer />}
-                    />
-                    <Route
-                        path="/welcomeUser"
-                        element={<Welcome />}
-                    />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LogingForm />} />
+          <Route path="/capacitaciones" element={<ItemsListContainer itemType={"training"} />} />
+          <Route path="/accesos" element={<ItemsListContainer itemType={"access"}/>} />
+          <Route path="/perfil" element={<Profile />} />
+          <Route path="/inicio" element={<Welcome />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/itemsList/ItemsListContainer.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ItemList from "./ItemList";
+
+function ItemsListContainer({ itemType }) {
+  if (itemType == "training") {
+    return (
+      <>
+        <div className="main-wrapper">
+          <div className="section-title">
+            <h1>Capacitaciones</h1>
+          </div>
+
+          <div className="accordion" id="accordionExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  Criticas
+                </button>
+              </h2>
+              <div
+                id="collapseOne"
+                className="accordion-collapse collapse show"
+                aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <ItemList />
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingTwo">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  Soft
+                </button>
+              </h2>
+              <div
+                id="collapseTwo"
+                className="accordion-collapse collapse"
+                aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <ItemList />
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingThree">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseThree"
+                  aria-expanded="false"
+                  aria-controls="collapseThree"
+                >
+                  Tecnicas
+                </button>
+              </h2>
+              <div
+                id="collapseThree"
+                className="accordion-collapse collapse"
+                aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <ItemList />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="main-wrapper">
+          <div className="section-title">
+            <h1>Accesos</h1>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+ItemsListContainer.propTypes = {};
+
+export default ItemsListContainer;
